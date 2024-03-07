d063b23fa33aaef1ba1dad398d72f35c93909a29
GioGioWasTaken <iyar.segev@gmail.com>
2024-03-07 11:13:02 +0200

    Delete unneeded file.

--- C/Networks/Chatapp/client.c
+++ C/Networks/Chatapp/client.c
@@ -5,23 +5,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/types.h>
+#include <unistd.h>
 #define BUFFER 100
+
 int main() {
   char message[BUFFER];
 
   int client_fd;
   client_fd = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr address = {AF_INET, htons(3440), 0};
-  // as programmed in server.c, the server is localhost, IPv4.
-  if (connect(client_fd, (struct sockaddr *)&address, sizeof(address) == -1)) {
-    perror("Connection failed");
+
+  struct addrinfo hints, *address_info;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;       // IPv4
+  hints.ai_socktype = SOCK_STREAM; // Stream socket
+
+  // Get address information for localhost and port 3440
+  int status = getaddrinfo("localhost", "3440", &hints, &address_info);
+  if (status != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
     exit(EXIT_FAILURE);
   }
-  while (strcmp(message, "Close")) {
-    send(client_fd, message, strlen(message), 0);
-    printf("Type message to server: \n");
-    fgets(message, BUFFER, stdin);
+
+  // Try connecting to the first available address
+  for (address_info = address_info; address_info != NULL;
+       address_info = address_info->ai_next) {
+    if (connect(client_fd, address_info->ai_addr, address_info->ai_addrlen) !=
+        -1) {
+      break; // Success, break the loop
+    }
   }
+
+  if (address_info == NULL) {
+    perror("Connection failed");
+    freeaddrinfo(address_info);
+    exit(EXIT_FAILURE);
+  }
+
+  // while (strcmp(message, "Close")) {
+  strcpy(message, "");
+  printf("Type message to server: \n");
+  fgets(message, BUFFER, stdin);
+  send(client_fd, message, strlen(message), 0);
+  //}
+
+  freeaddrinfo(address_info);
+
   return 0;
 }
