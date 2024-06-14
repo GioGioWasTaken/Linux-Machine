3983133579a7e3b348f5f94f77cc4ceb9d35254d
GioGioWasTaken <iyar.segev@gmail.com>
2024-06-14 13:09:05 +0300

    handle git corrupt

---
+++ Scripts/solvegitcorrupt.sh
@@ -0,0 +1,11 @@
+# cd .git
+# find . -type f -empty -delete -print
+# tail -n 2 .git/logs/refs/heads/master
+# # take first result
+# #
+# #git update-ref HEAD (second result)
+# rm .git/index
+# git reset
+
+
+# DONE
