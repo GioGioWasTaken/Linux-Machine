38ad38c07d5289a55921a3fa07cd8440c30b8090
GioGioWasTaken <iyar.segev@gmail.com>
2024-06-16 13:37:08 +0300

    more recursion

---
+++ Assembly/copy.s
@@ -0,0 +1,18 @@
+section .data
+msg db 'Hello, world!', 0xA  
+
+section .text
+global _start
+
+_start:
+    
+    mov eax, 4          
+    mov ebx, 1          
+    mov ecx, msg        
+    mov edx, 14         
+    int 0x80            
+
+    
+    mov eax, 1          
+    xor ebx, ebx        
+    int 0x80            
--- Java/Algorithms/Recursion/Coding_bat/recursion_2/recursion_3.java
+++ Java/Algorithms/Recursion/Coding_bat/recursion_2/recursion_3.java
@@ -4,12 +4,7 @@ public class recursion_3 {
       return (target == 0); // base case, if we have finished iterating over all
                             // elements, check if target is proper
     }
-    return groupNoAdj(
-        start + 2, nums,
-        target -
-            nums[start])
-        || // our choices: either including 1 and
-           // skipping the next, or not including one
+    return groupNoAdj( start + 2, nums, target - nums[start]) || // our choices: either including 1 and skipping the next, or not including one
         groupNoAdj(start + 1, nums, target);
   }
 }
