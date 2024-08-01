d296e609b1bf7015eab3e371ac2688ab2641a52a
GioGioWasTaken <iyar.segev@gmail.com>
2024-08-01 18:38:22 +0300

    started working on the assembler

---
+++ University/OS_C_prog/MMN_14_ASSEMBLER/Headers/preprocessor.h
@@ -0,0 +1,21 @@
+#ifndef PREPROCESSOR_H
+#define PREPROCESSOR_H
+#include <stdio.h>
+#include "utils.h"
+#include <string.h>
+#include "globals.h"
+#include <stdlib.h>
+
+typedef struct {
+    char name[MAX_MACRO_NAME];
+    char **lines; /* Will hold all lines that make a macro*/
+    int line_count;
+} Macro;
+
+
+FILE * preprocessor(char * src);
+
+FILE * Trim_file(FILE * src);
+
+int Save_macros(Macro *Macros, FILE* clean_file);
+#endif
